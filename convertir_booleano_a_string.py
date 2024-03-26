1a910feaf2779598ec5ca02a3bfba56d1b1ff67e
Gonzalez <alejogonzalez0341@gmail.com>
2024-03-25 21:47:30 -0500

    #1

---
+++ convertir_booleano_a_string.py
@@ -0,0 +1,24 @@
+def boolean_to_string(b):
+    # forma manual de hacerlo 
+
+    # se comprueba que el parametro "b" sea igual a True en valor booleano
+    if b == True:
+        #retorna una cadena de texto si la condición se cumple 
+        return "True"
+    
+        # de lo contrario pasa al else para cumplir con la siguiente tarea 
+    
+    # si el valor de el parametro "b" es un booleano False se retorna una cadena de texto con la palabra False
+    else:
+        "False"
+
+
+    ## formas simplificada de hacerlo obteniendo el mismo resultado ##
+        
+    """ return "True" if b else "False" ,
+     
+        return str(b)
+    
+       """
+
+
